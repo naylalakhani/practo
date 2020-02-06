0fff3299a5a3cca43d8a7a830290db0d02a3d87d
Hashmi Mohammed <hashmi.smf@gmail.com>
2020-02-06 17:33:09 +0530

    initialized and triggered class objects with its syntaxes

--- class.rb
+++ class.rb
@@ -1,11 +1,22 @@
 class Car
-  def initialize
-    start
-    move
-    accelerate_functioning
-    turns
-    arrive
-    stop
+  def initialize(name, hp, maxspeed)
+    puts "your car"  
+    puts "#{name} with #{hp} horsepower and maxspeed of #{maxspeed} is now ready"
   end
 
+  def start
+    puts 'vroooooom'
+  end
+
+  def stop
+    puts 'car is now off, you can lock and walk'
+  end
 end
+
+car = Car.new('breeza', 1500, 140)
+car.start
+puts 'car started'
+sleep(5)
+puts 'you are now stopping it'
+sleep(2)
+car.stop
